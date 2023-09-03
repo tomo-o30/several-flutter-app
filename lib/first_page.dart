2f808ef4cf8563d98f0eb717c459f4890755179f
tomo-o30 <tomo.ogawa18@gmail.com>
2023-09-03 15:49:53 +0900

    fix: 【Flutter超入門2022】③入力フォームに入れた値を次の画面に渡す

--- lib/first_page.dart
+++ lib/first_page.dart
@@ -2,23 +2,38 @@ import 'package:flutter/material.dart';
 import 'package:several_flutter_app/second_page.dart';
 
 class FirstPage extends StatelessWidget {
+  const FirstPage({super.key});
+
   @override
   Widget build(BuildContext context) {
+    String nameText = "";
+
     return Scaffold(
         appBar: AppBar(
           backgroundColor: Theme.of(context).colorScheme.inversePrimary,
           title: const Text("first"),
         ),
         body: Center(
-            child: ElevatedButton(
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                  builder: (context) => SecondPage(), fullscreenDialog: true),
-            );
-          },
-          child: const Text("次の画面へ"),
-        )));
+          child: Padding(
+            padding: const EdgeInsets.all(16),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextField(onChanged: (text) => {nameText = text}),
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => SecondPage(nameText),
+                          fullscreenDialog: true),
+                    );
+                  },
+                  child: const Text("次の画面へ"),
+                ),
+              ],
+            ),
+          ),
+        ));
   }
 }
--- lib/second_page.dart
+++ lib/second_page.dart
@@ -1,19 +1,36 @@
 import 'package:flutter/material.dart';
 
 class SecondPage extends StatelessWidget {
+  const SecondPage(this.name, {super.key});
+  final String name;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-          title: const Text("second"),
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: const Text("second"),
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                name,
+                style: const TextStyle(fontSize: 50),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: const Text("前の画面へ"),
+              ),
+            ],
+          ),
         ),
-        body: Center(
-            child: ElevatedButton(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          child: const Text("前の画面へ"),
-        )));
+      ),
+    );
   }
 }
